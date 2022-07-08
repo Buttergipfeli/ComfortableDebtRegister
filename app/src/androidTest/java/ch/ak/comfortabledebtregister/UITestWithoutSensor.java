f868e07c7047c5fc417555343710495ad7c4cd6c
Buttergipfeli <77845220+Buttergipfeli@users.noreply.github.com>
2022-07-08 10:22:08 +0200

    improved the generall UI-Test

--- app/src/androidTest/java/ch/ak/comfortabledebtregister/UITestWithoutSensor.java
+++ app/src/androidTest/java/ch/ak/comfortabledebtregister/UITestWithoutSensor.java
@@ -31,14 +31,14 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class UITest {
+public class UITestWithoutSensor {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void uITest() {
+    public void uITestWithoutSensor() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -68,6 +68,16 @@ public class UITest {
                         isDisplayed()));
         appCompatEditText2.perform(replaceText("Andre Kocher"), closeSoftKeyboard());
 
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.debtInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                2),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("5"), closeSoftKeyboard());
+
         ViewInteraction materialButton = onView(
                 allOf(withId(R.id.registerButton), withText("Registrieren"),
                         childAtPosition(
@@ -78,25 +88,35 @@ public class UITest {
                         isDisplayed()));
         materialButton.perform(click());
 
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.debtInput),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                2),
-                        isDisplayed()));
-        appCompatEditText3.perform(click());
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.debtInput),
+                allOf(withId(R.id.nameInput),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         2),
-                                2),
+                                0),
                         isDisplayed()));
-        appCompatEditText4.perform(replaceText("25"), closeSoftKeyboard());
+        appCompatEditText4.perform(replaceText("Dionis4"), closeSoftKeyboard());
 
         ViewInteraction materialButton2 = onView(
                 allOf(withId(R.id.registerButton), withText("Registrieren"),
@@ -108,65 +128,15 @@ public class UITest {
                         isDisplayed()));
         materialButton2.perform(click());
 
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(700);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        pressBack();
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(700);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         ViewInteraction appCompatEditText5 = onView(
-                allOf(withId(R.id.nameInput), withText("Andre Kocher"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                0),
-                        isDisplayed()));
-        appCompatEditText5.perform(replaceText("Hans Muster"));
-
-        ViewInteraction appCompatEditText6 = onView(
-                allOf(withId(R.id.nameInput), withText("Hans Muster"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                0),
-                        isDisplayed()));
-        appCompatEditText6.perform(closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText7 = onView(
-                allOf(withId(R.id.debtInput), withText("25"),
+                allOf(withId(R.id.debtInput),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         2),
                                 2),
                         isDisplayed()));
-        appCompatEditText7.perform(replaceText("25.5643"));
-
-        ViewInteraction appCompatEditText8 = onView(
-                allOf(withId(R.id.debtInput), withText("25.5643"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                2),
-                        isDisplayed()));
-        appCompatEditText8.perform(closeSoftKeyboard());
+        appCompatEditText5.perform(replaceText("0"), closeSoftKeyboard());
 
         ViewInteraction materialButton3 = onView(
                 allOf(withId(R.id.registerButton), withText("Registrieren"),
@@ -178,75 +148,25 @@ public class UITest {
                         isDisplayed()));
         materialButton3.perform(click());
 
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(700);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        pressBack();
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(700);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatEditText9 = onView(
-                allOf(withId(R.id.debtInput), withText("25.5643"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                2),
-                        isDisplayed()));
-        appCompatEditText9.perform(click());
-
-        ViewInteraction appCompatEditText10 = onView(
-                allOf(withId(R.id.debtInput), withText("25.5643"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                2),
-                        isDisplayed()));
-        appCompatEditText10.perform(replaceText("25.5"));
-
-        ViewInteraction appCompatEditText11 = onView(
-                allOf(withId(R.id.debtInput), withText("25.5"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        2),
-                                2),
-                        isDisplayed()));
-        appCompatEditText11.perform(closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText12 = onView(
-                allOf(withId(R.id.nameInput), withText("Hans Muster"),
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.nameInput), withText("Dionis4"),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         2),
                                 0),
                         isDisplayed()));
-        appCompatEditText12.perform(replaceText("Hans Johan"));
+        appCompatEditText6.perform(replaceText("Dionis"));
 
-        ViewInteraction appCompatEditText13 = onView(
-                allOf(withId(R.id.nameInput), withText("Hans Johan"),
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.nameInput), withText("Dionis"),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         2),
                                 0),
                         isDisplayed()));
-        appCompatEditText13.perform(closeSoftKeyboard());
+        appCompatEditText7.perform(closeSoftKeyboard());
 
         ViewInteraction materialButton4 = onView(
                 allOf(withId(R.id.registerButton), withText("Registrieren"),
@@ -258,6 +178,36 @@ public class UITest {
                         isDisplayed()));
         materialButton4.perform(click());
 
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.debtInput), withText("0"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                2),
+                        isDisplayed()));
+        appCompatEditText8.perform(replaceText("55.5"));
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.debtInput), withText("55.5"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                2),
+                        isDisplayed()));
+        appCompatEditText9.perform(closeSoftKeyboard());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.registerButton), withText("Registrieren"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        4),
+                                0),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -278,7 +228,107 @@ public class UITest {
             e.printStackTrace();
         }
 
-        ViewInteraction materialButton5 = onView(
+        ViewInteraction appCompatEditText10 = onView(
+                allOf(withId(R.id.nameInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                0),
+                        isDisplayed()));
+        appCompatEditText10.perform(replaceText("EinFreund"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText11 = onView(
+                allOf(withId(R.id.debtInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                2),
+                        isDisplayed()));
+        appCompatEditText11.perform(replaceText("45.24"), closeSoftKeyboard());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.registerButton), withText("Registrieren"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        4),
+                                0),
+                        isDisplayed()));
+        materialButton6.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText12 = onView(
+                allOf(withId(R.id.nameInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                0),
+                        isDisplayed()));
+        appCompatEditText12.perform(replaceText("Wie gehts"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText13 = onView(
+                allOf(withId(R.id.debtInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                2),
+                        isDisplayed()));
+        appCompatEditText13.perform(replaceText("3.42454656"), closeSoftKeyboard());
+
+        ViewInteraction materialButton7 = onView(
+                allOf(withId(R.id.registerButton), withText("Registrieren"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        4),
+                                0),
+                        isDisplayed()));
+        materialButton7.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialButton8 = onView(
                 allOf(withId(R.id.showButton), withText("Einträge"),
                         childAtPosition(
                                 childAtPosition(
@@ -286,7 +336,37 @@ public class UITest {
                                         4),
                                 2),
                         isDisplayed()));
-        materialButton5.perform(click());
+        materialButton8.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialButton9 = onView(
+                allOf(withId(R.id.showButton), withText("Einträge"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        4),
+                                2),
+                        isDisplayed()));
+        materialButton9.perform(click());
     }
 
     private static Matcher<View> childAtPosition(
